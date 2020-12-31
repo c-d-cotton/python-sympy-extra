365b854c103cee338c2718317bf58f33f30efb52
Christopher Cotton <cdc2175@columbia.edu>
2020-12-31 07:29:10 -0500

    Updated preambles

--- subs/test_subs_func.py
+++ subs/test_subs_func.py
@@ -1,55 +1,9 @@
 #!/usr/bin/env python3
-# PYTHON_PREAMBLE_START_STANDARD:{{{
-
-# Christopher David Cotton (c)
-# http://www.cdcotton.com
-
-# modules needed for preamble
-import importlib
 import os
 from pathlib import Path
 import sys
 
-# Get full real filename
-__fullrealfile__ = os.path.abspath(__file__)
-
-# Function to get git directory containing this file
-def getprojectdir(filename):
-    curlevel = filename
-    while curlevel is not '/':
-        curlevel = os.path.dirname(curlevel)
-        if os.path.exists(curlevel + '/.git/'):
-            return(curlevel + '/')
-    return(None)
-
-# Directory of project
-__projectdir__ = Path(getprojectdir(__fullrealfile__))
-
-# Function to call functions from files by their absolute path.
-# Imports modules if they've not already been imported
-# First argument is filename, second is function name, third is dictionary containing loaded modules.
-modulesdict = {}
-def importattr(modulefilename, func, modulesdict = modulesdict):
-    # get modulefilename as string to prevent problems in <= python3.5 with pathlib -> os
-    modulefilename = str(modulefilename)
-    # if function in this file
-    if modulefilename == __fullrealfile__:
-        return(eval(func))
-    else:
-        # add file to moduledict if not there already
-        if modulefilename not in modulesdict:
-            # check filename exists
-            if not os.path.isfile(modulefilename):
-                raise Exception('Module not exists: ' + modulefilename + '. Function: ' + func + '. Filename called from: ' + __fullrealfile__ + '.')
-            # add directory to path
-            sys.path.append(os.path.dirname(modulefilename))
-            # actually add module to moduledict
-            modulesdict[modulefilename] = importlib.import_module(''.join(os.path.basename(modulefilename).split('.')[: -1]))
-
-        # get the actual function from the file and return it
-        return(getattr(modulesdict[modulefilename], func))
-
-# PYTHON_PREAMBLE_END:}}}
+__projectdir__ = Path(os.path.dirname(os.path.realpath(__file__)) + '/../')
 
 def eqs():
     """
@@ -75,37 +29,44 @@ def eqs():
 
     print('My method - matrixsubsquick:')
     now = datetime.datetime.now()
-    matrix2 = importattr(__projectdir__ / Path('subs/subs_func.py'), 'matrixsubsquick')(matrix, replacedict)
+    from subs_func import matrixsubsquick
+    matrix2 = matrixsubsquick(matrix, replacedict)
     print(str(datetime.datetime.now() - now))
 
     print('My method2 - subsympymatrix_string:')
     now = datetime.datetime.now()
-    matrix2 = importattr(__projectdir__ / Path('subs/subs_func.py'), 'subsympymatrix_string')(matrix, replacedict)
+    from subs_func import subsympymatrix_string
+    matrix2 = subsympymatrix_string(matrix, replacedict)
     print(str(datetime.datetime.now() - now))
 
     print('My method3 - matrixofelements:')
     print('First part:')
     now = datetime.datetime.now()
-    matrixofelements = importattr(__projectdir__ / Path('subs/subs_func.py'), 'getmatrixofsympyelements')(matrix)
+    from subs_func import getmatrixofsympyelements
+    matrixofelements = getmatrixofsympyelements(matrix)
     print(str(datetime.datetime.now() - now))
     print('Second part:')
     now = datetime.datetime.now()
-    matrix2 = importattr(__projectdir__ / Path('subs/subs_func.py'), 'submatrixfrommatrixofelements')(matrix, replacedict, matrixofelements)
+    from subs_func import submatrixfrommatrixofelements
+    matrix2 = submatrixfrommatrixofelements(matrix, replacedict, matrixofelements)
     print(str(datetime.datetime.now() - now))
 
     print('lambdifysubs method:')
     now = datetime.datetime.now()
-    matrix2= importattr(__projectdir__ / Path('subs/subs_func.py'), 'lambdifysubs')(matrix, replacedict)
+    from subs_func import lambdifysubs
+    matrix2= lambdifysubs(matrix, replacedict)
     print(str(datetime.datetime.now() - now))
 
     print('lambdifysubs split method:')
     print('First part:')
     now = datetime.datetime.now()
-    matrix_f, sortedvars = importattr(__projectdir__ / Path('subs/subs_func.py'), 'lambdifysubs_lambdifyonly')(matrix, replacedict)
+    from subs_func import lambdifysubs_lambdifyonly
+    matrix_f, sortedvars = lambdifysubs_lambdifyonly(matrix, replacedict)
     print(str(datetime.datetime.now() - now))
     print('Second part:')
     now = datetime.datetime.now()
-    matrix2 = importattr(__projectdir__ / Path('subs/subs_func.py'), 'lambdifysubs_subsonly')(matrix_f, sortedvars, replacedict)
+    from subs_func import lambdifysubs_subsonly
+    matrix2 = lambdifysubs_subsonly(matrix_f, sortedvars, replacedict)
     print(str(datetime.datetime.now() - now))
 
     print('sympy evalf method:')    
